096ee385b06fb215194ca4052daf89f9feb9bcfe
Diego Prats <diego.prats@gmail.com>
2015-08-01 20:30:33 -0700

    finish class 2

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -13,9 +13,28 @@ angular.module('templateStoreApp.templates', ['ngRoute'])
 
 }]).
 
-controller('TemplatesCtrl', ['$scope', function($scope){
+controller('TemplatesCtrl', ['$scope','$http', function($scope, $http){
+
+	$http.get('json/templates.json').success(function(response){
+		$scope.templates = response;
+	});
+
 }])
 
-.controller('TemplateDetailsCtrl', ['$scope', function($scope){
+.controller('TemplateDetailsCtrl', ['$scope', '$routeParams', '$http', '$filter', function($scope,$routeParams, $http, $filter){
+
+	var templateId = $routeParams.templateId;
+
+	$http.get('json/templates.json').success(function(response){
+		$scope.template = $filter('filter')(response, function(d){
+			return d.id == templateId;
+		})[0];
+
+		$scope.mainImage = $scope.template.images[0].name;
+	});
+
+	$scope.setImage = function(image){
+		$scope.mainImage = image.name;
+	}
 
 }]);
